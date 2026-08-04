e305fd0759a6045b4586534728f8da81920fd5d7
Zerofivem <zerofivem@gmail.com>
2026-08-04 14:04:53 +0800

    feat: export expanded strategy registry

--- src/strategy-engine-v2/index.ts
+++ src/strategy-engine-v2/index.ts
@@ -1,3 +1,8 @@
 export * from './types';
 export * from './engine';
-export { STRATEGY_PROFILES_V2 } from './data';
+export * from './importer';
+
+import { STRATEGY_PROFILES_V2 as RFI_PROFILES } from './data';
+import { ADVANCED_STRATEGY_PROFILES } from './advancedData';
+
+export const STRATEGY_PROFILES_V2 = [...RFI_PROFILES, ...ADVANCED_STRATEGY_PROFILES];
