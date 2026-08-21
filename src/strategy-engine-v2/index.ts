6a626405f630b685efa1bef669715f50f58d2f15
Zerofivem <zerofivem@gmail.com>
2026-08-21 11:49:07 +0800

    Export unified truth coverage portfolio

--- src/strategy-engine-v2/index.ts
+++ src/strategy-engine-v2/index.ts
@@ -6,6 +6,7 @@ export * from './exploit';
 export * from './population';
 export * from './populationRegistry';
 export * from './coverage';
+export * from './truthPortfolio';
 
 import { STRATEGY_PROFILES_V2 as RFI_PROFILES } from './data';
 import { ADVANCED_STRATEGY_PROFILES } from './advancedData';
